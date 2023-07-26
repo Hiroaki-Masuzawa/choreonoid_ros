5d7b2a1cd4692966060d7083e649cd1d327d7e3b
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.ac.jp>
2023-07-26 16:41:35 +0900

    change to publish color image

--- src/plugin/BodyROSItem.cpp
+++ src/plugin/BodyROSItem.cpp
@@ -165,21 +165,7 @@ void BodyROSItem::createSensors(BodyPtr body)
         }
     }
 
-    auto itr = visionSensors_.begin();
-    while(itr != visionSensors_.end()){
-        if (Camera* sensor = *itr){
-            RangeCamera* camera = dynamic_cast<RangeCamera*>(sensor);
-            if (sensor->imageType() == cnoid::Camera::NO_IMAGE) {
-                itr = visionSensors_.erase(itr);
-            } else if (camera && !camera->isOrganized()) {
-                itr = visionSensors_.erase(itr);
-            }else{
-                ++itr;
-            }
-        }else{
-            ++itr;
-        }
-    }
+
 
     forceSensorPublishers.clear();
     forceSensorPublishers.reserve(forceSensors_.size());
